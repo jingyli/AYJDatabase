8ff58352e02cc05caf97afa69496879d34165367
jingyli <jingyi.li0310@gmail.com>
2016-01-15 19:55:16 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,9 +1,5 @@
 /*
 File Name: Student.java
-Course: ICS4U1-01
-Teacher Name: Ms. Zeng
-Name: Jing Li & Nicole Zhang
-Date: December 22, 2014
 Description: The following program creates a Student object and has all student-related functions.
 */
 
